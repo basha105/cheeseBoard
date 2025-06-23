5d829cae495e8fefb062032824c94fa90cc8bf27
basha105 <brentbasha17@gmail.com>
2025-06-22 18:36:02 -0700

    moved sign-in button in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,12 +3,17 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <ul className="h-8 flex text-blue-400 bg-white p-1 shadow">
-            <li className="hover:bg-gray-100 px-2 rounded font-bold"><Link to="/">cheeseBoard</Link></li>
-            <li className="hover:bg-gray-100 px-2 rounded"><Link>reviews</Link></li>
-            <li className="hover:bg-gray-100 px-2 rounded"><Link to="/cheeses">cheeses</Link></li>
-            <li className="hover:bg-gray-100 px-2 rounded"><Link>sign in</Link></li>
-        </ul>
+        <div className="h-8 flex justify-between text-blue-400 bg-white p-1 shadow">
+            <ul className="flex">
+                <li className="hover:bg-gray-100 px-2 rounded font-bold"><Link to="/">cheeseBoard</Link></li>
+                <li className="hover:bg-gray-100 px-2 rounded"><Link>reviews</Link></li>
+                <li className="hover:bg-gray-100 px-2 rounded"><Link to="/cheeses">cheeses</Link></li>
+            </ul>
+            <div className="mr-1 hover:bg-gray-100 px-2 rounded"><Link>sign in</Link></div>
+            
+        </div>
+        
+    
     )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
             <Navbar />
             <div className="m-12 flex flex-col items-center">
                 <div className="text-4xl mb-2">Welcome to <span className="font-semibold text-blue-400">cheeseBoard</span></div>
-                <div>cheeseBoard is a community dedicated to all things cheese. Rate, review, and track your cheese journey!</div>
+                <div className="mb-2">cheeseBoard is a community dedicated to all things cheese. Rate, review, and track your cheese journey!</div>
                 <div>"Poets have been mysteriously silent on the subject of cheese." - G.K. Chesterton</div>
             </div>
         </div>
