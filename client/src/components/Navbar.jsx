128dbac2e4faaca2c653378f03b80536d9837b49
basha105 <brentbasha17@gmail.com>
2025-06-22 22:44:20 -0700

    implemented not yet functional search bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <div className="h-8 flex justify-between text-blue-400 bg-white p-1 shadow">
-            <ul className="flex">
+        <div className="h-10 flex text-blue-400 bg-white p-1 shadow">
+            <ul className="flex align-middle">
                 <li className="hover:bg-gray-100 px-2 rounded font-bold"><Link to="/">cheeseBoard</Link></li>
                 <li className="hover:bg-gray-100 px-2 rounded"><Link>reviews</Link></li>
                 <li className="hover:bg-gray-100 px-2 rounded"><Link to="/cheeses">cheeses</Link></li>
+                <li className="w-250">
+                    <form className="" action="">
+                        <label for="searchEntry"></label>
+                        <input className="outline-none p-0.5 border-1 w-250 rounded border-gray-300" type="text" name="searchEntry" id="searchEntry" placeholder="search for any cheese..."/>
+                    </form>
+                </li>
             </ul>
             <div className="mr-1 hover:bg-gray-100 px-2 rounded"><Link>sign in</Link></div>
             
