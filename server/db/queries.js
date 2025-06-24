a7a48b368a1fe026f32cb309ede99e35ce70ed7c
basha105 <brentbasha17@gmail.com>
2025-06-23 22:39:29 -0700

    added db func

--- server/db/queries.js
+++ server/db/queries.js
@@ -10,6 +10,11 @@ async function getCheeses() {
     return cheeses;
 }
 
+async function getCheesesByCountry(country){
+    const cheeses = await pool.query("SELECT cheeses.name, countries.country_name INNER JOIN countries ON cheeses.country_id=countries.country_id WHERE countries.country_name = $1", [country]);
+    return cheeses;
+}
+
 async function getCheeseByName(name) {
     const cheese = await pool.query(
         'SELECT cheeses.name, countries.country_name FROM cheeses INNER JOIN countries ON cheeses.country_id=countries.country_id WHERE cheeses.name = $1', [name]
@@ -25,6 +30,7 @@ async function getCheeseById(id) {
 module.exports = {
     getCountries,
     getCheeses,
+    getCheesesByCountry,
     getCheeseByName,
     getCheeseById
 }
