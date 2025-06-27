8e0cbfd8f44de961850cbc81ae263f638b628ecc
basha105 <brentbasha17@gmail.com>
2025-06-26 17:54:47 -0700

    working

--- server/controllers/passportController.js
+++ server/controllers/passportController.js
@@ -16,3 +16,4 @@ async function getUserById(id) {
     return rows;
 
 }
+
--- server/db/queries.js
+++ server/db/queries.js
@@ -35,8 +35,8 @@ async function insertUser(username, password) {
 }
 
 async function getUserById(id) {
-    const user = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
-    return user;
+    const rows = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    return rows;
 }
 
 module.exports = {
